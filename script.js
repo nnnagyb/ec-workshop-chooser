640ed5e525c109b498ca4710f3bbc409a4f34717
nnnagyb <nnnagyb@student.elte.hu>
2025-07-18 23:46:33 +0200

    button colors, popups, background image

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ function openTab(tabID, closeGroup) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  document.getElementById(tabID).style.display = "block";
+  var y = document.getElementsByClassName(closeGroup + "-button");
+  for (i = 0; i < y.length; i++) {
+    y[i].classList.remove("active");
+  }
+
+  document.getElementById(tabID).style.display = "flex";
+  document.getElementById(tabID + "-button").classList.add("active");
 }
